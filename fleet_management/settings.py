968efb0c34597fa7b640ce66ca501cf8ca30af88
biancaBezerra <bianca.skills@gmail.com>
2024-04-04 11:20:21 -0300

    T001: feat - Configuração do Migrate e instalação de dependencias

--- fleet_management/settings.py
+++ fleet_management/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'fleet_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fleet_management_api',
+        'USER': 'postgres',
+        'PASSWORD': '4401',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
